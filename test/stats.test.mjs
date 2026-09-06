822f5eb4e018f18ee156d9ee1fea469696b70ba4
cadp-worker <worker@cadp-v04.invalid>
2026-09-06 13:07:25 +0900

    cadp candidate: In src/stats.mjs, add and export a function `range(xs)` that

--- test/stats.test.mjs
+++ test/stats.test.mjs
@@ -1,6 +1,6 @@
 import assert from "node:assert/strict";
 import test from "node:test";
-import { mean, median } from "../src/stats.mjs";
+import { mean, median, range } from "../src/stats.mjs";
 
 test("mean of empty is 0", () => assert.equal(mean([]), 0));
 test("mean averages", () => assert.equal(mean([2, 4]), 3));
@@ -8,3 +8,7 @@ test("mean averages", () => assert.equal(mean([2, 4]), 3));
 test("median of empty is 0", () => assert.equal(median([]), 0));
 test("median of odd length", () => assert.equal(median([3, 1, 2]), 2));
 test("median of even length", () => assert.equal(median([4, 1, 3, 2]), 2.5));
+
+test("range of empty is 0", () => assert.equal(range([]), 0));
+test("range of single element is 0", () => assert.equal(range([4]), 0));
+test("range of unsorted values", () => assert.equal(range([4, 1, 3, 2]), 3));
